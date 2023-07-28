c4213812482d1df6210981faa7cc094600eeec10
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-07-28 20:27:41 +0530

    Updated App.js for mentor route using AutoPilot

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const PORT = 8009;
 
 require('dotenv').config();
 
+const mentorRoute = require('./routes/mentor');
+app.use('/mentor', mentorRoute);
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
